df79152de40ee299af73ec1340285eaa954371b2
Stefan <stefan.heid@lmu.de>
2024-02-19 17:07:14 +0100

    minor

--- experiments/util/dataloader.py
+++ experiments/util/dataloader.py
@@ -15,7 +15,6 @@ class DataLoader:
             df = pd.read_csv(file)
             return df.iloc[:, :-1].to_numpy(), df.iloc[:, -1].to_numpy()
 
-
         X, y = fetch_openml(data_id=dataset, return_X_y=True, as_frame=False)
         y = np.array(y == np.unique(y)[0], dtype=int)
         pd.DataFrame(np.hstack((X, y.reshape(-1, 1)))).to_csv(file, index=False)
